6ec833b338eee1af781cd1e8cb488f6af36f67d0
Zach <bardown14@hotmail.com>
2021-10-22 14:12:30 -0500

    initial commit of changes

---
+++ csharp/GildedRose.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQualityNew()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                int baseDecay = 1;
+
+                switch (Items[i].Name) {
+                    case "Canned Beans":
+                        break;
+                    case string s when s.StartsWith("Baked"):
+                        if (Items[i].SellIn > 0)
+                        {
+                            Items[i].Quality = decay(Items[i].Quality, baseDecay * 2);
+                        }
+                        else
+                        {
+                            Items[i].Quality = expiredDecay(Items[i].Quality, baseDecay * 2);
+                        }
+                        break;
+                    case "Aged Brie":
+                        if (Items[i].SellIn > 0)
+                        {
+                            Items[i].Quality = decay(Items[i].Quality, baseDecay * -1);
+                        }
+                        else
+                        {
+                            Items[i].Quality = expiredDecay(Items[i].Quality, baseDecay * -1);
+                        }
+                        break;
+                    default:
+                        if (Items[i].SellIn > 0)
+                        {
+                            Items[i].Quality = decay(Items[i].Quality, baseDecay);
+                        }
+                        else
+                        {
+                            Items[i].Quality = expiredDecay(Items[i].Quality, baseDecay);
+                        }
+                        break;
+                }
+                
+                Items[i].SellIn -= 1;
+            }
+        }
+
+        private static int decay(int itemValue, int decayValue) {
+            int value = itemValue;
+            if (itemValue >= 0 && itemValue <= 50)
+            {
+                value = itemValue - decayValue;
+            }
+            if (value < 0)
+                value = 0;
+            if (value > 50)
+                value = 50;
+            return value;
+        }
+        private static int expiredDecay(int itemValue, int decayValue) {
+            int value = itemValue;
+            if (itemValue >= 0 && itemValue <= 50)
+            {
+                value = itemValue - (decayValue * 2);
+            }
+            if (value < 0)
+                value = 0;
+            if (value > 50)
+                value = 50;
+            return value;
+        }
+    }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    public class Program
+    {
+        private const int Days = 20;
+
+        public static void Main(string[] args)
+        {
+            IList<Item> Items2 = new List<Item>{
+                new Item {Name = "Bananas", SellIn = 2, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 50},
+                new Item {Name = "Eggs", SellIn = 2, Quality = 7},
+                new Item {Name = "Eggs", SellIn = 12, Quality = 5},
+                new Item {Name = "Canned Beans", SellIn = 0, Quality = 80},
+                new Item {Name = "Canned Beans", SellIn = -1, Quality = 80},
+                
+                // This Baked good does not work properly yet!
+                new Item {Name = "Baked Sourdough Bread", SellIn = 3, Quality = 6},
+                new Item {Name = "Baked Beans", SellIn = 2, Quality = 40 }
+            };
+
+            var app2 = new GildedRose(Items2);
+
+            for (var i = 0; i < (Days + 1); i++)
+            {
+                Console.WriteLine("-------- day " + i + " --------");
+                Console.WriteLine("name, sellIn, quality");
+                for (var j = 0; j < Items2.Count; j++)
+                {
+                    System.Console.WriteLine(Items2[j].Name + ", " + Items2[j].SellIn + ", " + Items2[j].Quality);
+                }
+                Console.WriteLine("");
+                app2.UpdateQualityNew();
+            }
+            Console.ReadKey();
+        }
+    }
+}
